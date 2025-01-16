e814175372c511557d4e276d5c231e9fb9816054
aidys23 <aidongak23@gmail.com>
2025-01-16 12:33:29 +0700

    change done? predict

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -14,4 +14,10 @@ RSpec.describe Project do
     puts task.frozen?
     expect(project).not_to be_done
   end
+
+  it "marks a project done if its tasks are done" do
+    project.tasks << task
+    task.mark_completed
+    expect(project).to be_done
+  end
 end
