5433822bbd9a54aa35cba0fceca0402cd5b02386
aidys23 <aidongak23@gmail.com>
2025-01-16 11:41:45 +0700

    add project

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Project do
+  let(:project) { Project.new }
+  let(:task) { Task.new }
+  
+  it 'consider a project with no tasks to be done' do
+    expect(project.done?).to be_truthy
+  end
+
+  it 'knows that a project with an incomplete task is not done' do
+    project.tasks << task
+
+    puts task.frozen?
+    expect(project).not_to be_done
+  end
+end
