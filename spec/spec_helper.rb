ff79a18b9bb0ff21e82a287c9e441ab643f77dee
aidys23 <aidongak23@gmail.com>
2025-01-21 13:58:48 +0700

    config spec: random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,7 @@ RSpec.configure do |config|
 
 
   config.example_status_persistence_file_path = "spec/examples.txt"
+  config.order = :random
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
