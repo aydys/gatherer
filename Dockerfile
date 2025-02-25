bcc33337818af4a3f3543ad355caf17f82e2df20
aidys23 <aidongak23@gmail.com>
2025-02-25 22:57:09 +0700

    testing js: integration testing

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM ruby:2.5
 
+RUN apt-get update && \
+    apt-get install -y curl && \
+    curl -sL https://deb.nodesource.com/setup_8.x | bash - && \
+    apt-get install -y nodejs && \
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+    apt-get update && \
+    apt-get install -y yarn && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 COPY Gemfile* /usr/app/
 
 WORKDIR /usr/app
--- spec/support/system.rb
+++ spec/support/system.rb
@@ -1,6 +1,24 @@
+require 'selenium/webdriver'
+
+Capybara.register_driver :headless_selenium_chrome_in_container do |app|
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    url: 'http://selenium_chrome:4444/wd/hub',
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless disable-gpu) }
+    )
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :headless_selenium_chrome_in_container
+    Capybara.server_host = '0.0.0.0'
+    Capybara.server_port = 4000
+    Capybara.app_host = "http://web:4000"
   end
 end
 
